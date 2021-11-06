d5609ec5e96b2a63ed223c9bed725e8ca5c1efd6
Scott Xu <fgsoap@gmail.com>
2021-11-06 13:56:50 +0800

    add LVB

--- lvb.py
+++ lvb.py
@@ -1,2 +1,12 @@
 class RazPlus:
     pass
+
+class LVB:
+    def get_audio():
+        pass
+    def get_image():
+        pass
+    def concat_audios_and_images():
+        pass
+    def concat_videos():
+        pass
