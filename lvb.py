51e441d559d46d7a227b10cb67995b1651d4cebe
Scott Xu <fgsoap@gmail.com>
2021-11-06 22:53:59 +0800

    init username & password

--- lvb.py
+++ lvb.py
@@ -1,12 +1,21 @@
 class RazPlus:
-    pass
+    def __init__(self, username, password):
+        self.username = username
+        self.passowrd = password
+
+    def login():
+        pass
+
 
 class LVB:
     def get_audio():
         pass
+
     def get_image():
         pass
+
     def concat_audios_and_images():
         pass
+
     def concat_videos():
         pass
