0245e251c5083d76c33fd8e70e675591f9165c8b
Scott Xu <fgsoap@gmail.com>
2021-11-07 11:27:58 +0800

    add example to get_image

--- lvb.py
+++ lvb.py
@@ -1,4 +1,4 @@
-import requests
+import requests, shutil
 
 
 class RazPlus(object):
@@ -7,29 +7,46 @@ class RazPlus(object):
         self.passowrd = password
 
     def login(self):
-        r = requests.post('https://www.raz-plus.com/auth/login.php',
-                          data={
-                              'username': self.username,
-                              'password': self.passowrd
-                          },
-                          allow_redirects=True)
-        print(r.status_code)
+        s = requests.Session()
+        r = s.post('https://www.raz-plus.com/auth/login.php',
+                   data={
+                       'username': self.username,
+                       'password': self.passowrd
+                   },
+                   allow_redirects=True)
+        if r.status_code == 200:
+            return s
+        else:
+            raise Exception('Bad Response!')
 
 
 class LVB(object):
-    def get_audio():
+    def __init__(self, session, id):
+        self.session = session
+        self.id = id
+
+    def get_audio(self):
         pass
 
-    def get_image():
+    def get_image(self):
+        r = self.session.get(
+            'https://cf.content.raz-plus.com/raz_book_image/2879/projectable/large/1/book/page-3.jpg',
+            stream=True)
+        if r.status_code == 200:
+            with open('2879.jpg', 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+
+    def concat_audios_and_images(self):
         pass
 
-    def concat_audios_and_images():
-        pass
-
-    def concat_videos():
+    def concat_videos(self):
         pass
 
 
 if __name__ == '__main__':
-    razplus = RazPlus('', '')
-    razplus.login()
+    razplus = RazPlus('nutrokalmu', 'xZtDcCsP')
+    s = razplus.login()
+
+    lvb = LVB(s, 2879)
+    lvb.get_image()
