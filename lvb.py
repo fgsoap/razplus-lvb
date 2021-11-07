cb2ad4a4d728a02392f87f46c898750f431d22de
Scott Xu <fgsoap@gmail.com>
2021-11-07 10:52:51 +0800

    add login

--- lvb.py
+++ lvb.py
@@ -1,13 +1,22 @@
-class RazPlus:
+import requests
+
+
+class RazPlus(object):
     def __init__(self, username, password):
         self.username = username
         self.passowrd = password
 
-    def login():
-        pass
+    def login(self):
+        r = requests.post('https://www.raz-plus.com/auth/login.php',
+                          data={
+                              'username': self.username,
+                              'password': self.passowrd
+                          },
+                          allow_redirects=True)
+        print(r.status_code)
 
 
-class LVB:
+class LVB(object):
     def get_audio():
         pass
 
@@ -19,3 +28,8 @@ class LVB:
 
     def concat_videos():
         pass
+
+
+if __name__ == '__main__':
+    razplus = RazPlus('', '')
+    razplus.login()
