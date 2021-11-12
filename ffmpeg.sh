f699e7b7d238d74bef28c556e52e7a9f88bdac2b
Scott Xu <fgsoap@gmail.com>
2021-11-12 10:01:57 +0800

    create mylist in runtime

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -5,4 +5,9 @@ do
 ffmpeg -loop 1 -safe 0 -i $i.jpg -i $i.mp3 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -x264-params keyint=1:scenecut=0 -c:a copy -shortest $i.mp4
 done
 
+for i in *.mp4
+do
+    echo "file '$i'"
+done > mylist.txt
+
 ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp4
