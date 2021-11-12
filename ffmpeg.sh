7fb5c7f9be08854c7447aef1bbe5a47b97c9b63a
Scott Xu <fgsoap@gmail.com>
2021-11-12 14:12:31 +0800

    fix

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -5,4 +5,5 @@ do
 ffmpeg -safe 0 -loop 1 -i $i.jpg -i $i.mp3 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -x264-params keyint=1:scenecut=0 -c:a copy -shortest $i.mp4
 done
 
-ffmpeg -safe 0 -f concat -i <(find . -type f -name '*.txt' -printf "file '$PWD/%p'\n" | sort)> -c copy output.mp4
+find . -type f -name '*.txt' -printf "file '$PWD/%p'\n" | sort > mylist.txt
+ffmpeg -safe 0 -f concat -i 'mylist.txt'' -c copy output.mp4
