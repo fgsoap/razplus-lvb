5053029110245cb3da303f34c5d15e3a1a227ede
Scott Xu <fgsoap@gmail.com>
2021-11-12 20:20:50 +0800

    fix

--- ffmpeg_merge.sh
+++ ffmpeg_merge.sh
@@ -2,5 +2,5 @@
   
 for i in $(seq 3 10)  
 do   
-ffmpeg -safe 0 -loop 1 -i $i.jpg -i $i.mp3 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -x264-params keyint=1:scenecut=0 -c:a copy -shortest $i.mp4
+ffmpeg -loop 1 -i $i.jpg -i $i.mp3 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -c:v libx264 -x264-params keyint=1:scenecut=0 -c:a copy -shortest $i.mp4
 done
